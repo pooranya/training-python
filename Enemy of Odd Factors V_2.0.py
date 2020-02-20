9c41cbe7d4a099555faf238e17fd704ff942e2d1
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-20 11:04:37 +0530

    Create Enemy of Odd Factors V_2.0.py

---
+++ Enemy of Odd Factors V_2.0.py
@@ -0,0 +1,27 @@
+#question
+In the previous Version of the question you did help to find whether the given number is enemy or a friend to the odd factor. Here we just go one step further and help find how many number which divide the given number are enemies of odd factors. A number is said to be enemy of the odd factor if the number itself is not having odd number of factors and none of the factors of the number(excluding 1) are having odd number of factors.
+
+Input Format
+
+One single integer N
+
+Constraints
+
+N<=10^9 Given number N will not be divisible by any prime number more than 19
+
+Output Format
+
+One single integer
+
+Sample Input 0
+
+20
+
+Sample Output 0
+
+3
+
+Explanation 0
+
+2 5 10 these three numbers which diveide 20 are enemies of odd factors
+
