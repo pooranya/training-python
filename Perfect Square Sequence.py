97ed10030040a89ead52461f7a712de65a23293c
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-20 18:03:20 +0530

    Create  Perfect Square Sequence.py

---
+++ Perfect Square Sequence.py
@@ -0,0 +1,31 @@
+#question
+For Given a positive integer 'N', print the list of perfect square numbers that could divide the given number 'N'.
+
+Input Format
+
+Single positive integer 'N'
+
+Constraints
+
+N<10^9
+
+Output Format
+
+All perfect Square numbers that could divide 'N' evenly.
+
+Sample Input 0
+
+7236
+
+Sample Output 0
+
+1 4 9 36
+
+Sample Input 1
+
+20
+
+Sample Output 1
+
+1 4
+
