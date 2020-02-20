e829b46e9235a8520a0801306489a2f7b4649a50
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-20 09:50:30 +0530

    Update enemy of odd factor.py

--- enemy of odd factor.py
+++ enemy of odd factor.py
@@ -1,3 +1,10 @@
+from math import sqrt as s
+a=int(input())
+if s(a)%1==0:
+    print("Enemy of Odd Factor")
+else:
+    print("Friend of Odd Factor")
+    
 #question
 Given a number N, Check whether the given number is enemy of Odd factor or not. A number is said to be enemy of odd factors if the number itself shoud not have odd number of factors and none of the factors (excluding 1) should have odd number of factors, If so then print "Enemy of Odd Factor" else print "Friend of Odd Factor".
 
