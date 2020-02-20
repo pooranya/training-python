418879fc735f4900812991f8bc2a7cde6315c598
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-20 09:05:11 +0530

    Create enemy of odd factor.py

---
+++ enemy of odd factor.py
@@ -0,0 +1,14 @@
+#question
+Given a number N, Check whether the given number is enemy of Odd factor or not. A number is said to be enemy of odd factors if the number itself shoud not have odd number of factors and none of the factors (excluding 1) should have odd number of factors, If so then print "Enemy of Odd Factor" else print "Friend of Odd Factor".
+
+Input Format
+
+One single integer N
+
+Constraints
+
+1<=N<=10^9
+
+Output Format
+
+Enemy of Odd Factor / Friend of Odd Factor
