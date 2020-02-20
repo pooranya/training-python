b01dc9d0687eef2b3ed35282802ada24e988e70f
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-20 20:46:53 +0530

    Update 1,4,5,16.....sequence.py

--- 1,4,5,16.....sequence.py
+++ 1,4,5,16.....sequence.py
@@ -7,3 +7,15 @@ while(a):
     c+=1
     a=a>>1
 print(s)
+#answer2
+n=int(input())
+s=bin(n)[2:]
+j=" "
+for i in range(0,len(s)):
+    if i%2==0:
+        j+=s[i]       
+a=int(j,2)
+if n==int(j,4):
+    print(a)
+else:
+print("NOT PRESENT")
