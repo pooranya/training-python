919d11c5f645b686922f59050f3d260c713da213
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 18:05:02 +0530

    Create  Expression Length.py

---
+++ Expression Length.py
@@ -0,0 +1,57 @@
+#que
+For the given two integers 'a' and 'b' print the number of digits that will be present in a^b
+
+Input Format
+
+Two lines. First line contains integer 'a' Second line contains integer 'b'
+
+Constraints
+
+a,b<10^5
+
+Output Format
+
+single integer representing no. of Digits in the expression
+
+Sample Input 0
+
+8
+5
+
+Sample Output 0
+
+5
+
+Explanation 0
+
+8**5 = 32768
+
+Hence, No. of Digits is 5
+
+Sample Input 1
+
+4
+3
+
+Sample Output 1
+
+2
+
+Explanation 1
+
+4**3 = 64
+
+Hence, No. of Digits is 2
+
+Submissions: 19
+
+Max Score:
+
+30
+
+Difficulty:
+
+Medium
+
+Rate This Challenge:
+More
