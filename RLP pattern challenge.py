d47385fc2dbf79cd15a34b948bb026d0cfd16251
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 18:03:10 +0530

    Create RLP pattern challenge.py

---
+++ RLP pattern challenge.py
@@ -0,0 +1,31 @@
+#que
+For the given STRING input 'S' print the following pattern in order.
+
+Example: N = ZOHO
+
+image
+
+Input Format
+
+Single String 'S'
+
+Constraints
+
+LENGTH OF STRING < 10
+
+Output Format
+
+Shape Pattern
+
+Sample Input 0
+
+RLP
+
+Sample Output 0
+
+    R    
+   RLR   
+  RLPLR  
+ RLLLLLR 
+RRRRRRRRR
+
