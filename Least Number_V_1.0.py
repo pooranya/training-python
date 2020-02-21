d4322d13b24eaa79db5c4c82b99a85ecaa02c89b
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 10:18:26 +0530

    Update Least Number_V_1.0.py

--- Least Number_V_1.0.py
+++ Least Number_V_1.0.py
@@ -1,4 +1,39 @@
-question
+def f(a):
+    k=1
+    for i in range(2,a+1):
+        k*=i
+    return k
+n=input()
+arr=f(len(n))
+m=[]
+for i in n:
+    if i not in m:
+        m.append(i)
+div=1
+for i in m:
+    div=div*f(n.count(i))
+pos=arr//div
+rep=pos//len(n)
+s=0
+for i in n:
+    s+=int(i)
+s=s*rep
+mul=1
+val=0
+for i in range(len(n)):
+    val+=s*mul
+    mul*=10
+print(val)
+if(len(m)==1):
+    print("".join(n))
+else:
+    print(val)
+
+
+
+
+
+#question
 Given a number 'N', print the least possibe number that could be formed by removing 'K' digits.(The order of the elements should not be altered)
 
 Input Format
