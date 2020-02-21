92b6e84fe2388f0d747db1f0c7c7b55da4935927
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 09:04:48 +0530

    Create  Combined Sum.py

---
+++ Combined Sum.py
@@ -0,0 +1,35 @@
+question
+Given a number N, find the sum of all different arrangements of N and print that as the final answer.
+
+Input Format
+
+A sngle integer N
+
+Constraints
+
+1<=N<=10^9
+
+Output Format
+
+One integer denoting the sum of all possible arrangements of the number N
+
+Sample Input 0
+
+123
+
+Sample Output 0
+
+1332
+
+Explanation 0
+
+The Possibilities are 
+
+123
+132
+213
+231
+312
+321
+
+Whose sum will be 1332
