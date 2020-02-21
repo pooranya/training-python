70fe441356bef49d069f0ae7ead3e4c22df636e3
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 18:02:21 +0530

    Create odd love.py

---
+++ odd love.py
@@ -0,0 +1,31 @@
+#que
+Given a number N, find the least possible number x which divides your number such that the qoutient is an odd number.
+
+Input Format
+
+One single integer
+
+Constraints
+
+1<=N<=10^18
+
+Output Format
+
+Least value of x
+
+Sample Input 0
+
+8
+
+Sample Output 0
+
+8
+
+Sample Input 1
+
+15
+
+Sample Output 1
+
+1
+
