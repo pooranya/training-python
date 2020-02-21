93fc0e19268641cdf466a36f9742c18528593303
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-21 18:04:02 +0530

    Create  Sorted Duplicates.py

---
+++ Sorted Duplicates.py
@@ -0,0 +1,37 @@
+#que
+From a list of integers, print only the numbers which has count>1 in sorted ascending order.
+
+Input Format
+
+first line contains single integer 'N' denoting the size of the array.
+
+second line contains 'N' spaced integers.
+
+Constraints
+
+10<=N<=100
+
+each arr[i]<=100
+
+Output Format
+
+'K' spaced integers sorted in ascending order
+
+Sample Input 0
+
+14
+0 0 2 0 0 4 3 2 4 0 1 0 0 1
+
+Sample Output 0
+
+0 1 2 4
+
+Sample Input 1
+
+15
+5 1 1 5 0 3 5 0 1 1 1 0 3 4 1
+
+Sample Output 1
+
+0 1 3 5
+
