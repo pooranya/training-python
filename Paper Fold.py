c10994cbdccc9070c969222339bec09c5993a0f0
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-22 17:32:58 +0530

    Create  Paper Fold.py

---
+++ Paper Fold.py
@@ -0,0 +1,22 @@
+A piece of paper cannot be folded into half more than 7 times with bare hands. So to overcome this, the company RelearnPro has desighned a machine to fold the paper as many times you want by generating immesnse force. So given a piece of paper with a thickness 'T' mm, and after folding the paper into half for 'N' times, find what will be size of the paper in mm.
+
+Input Format
+
+Two spaced integers T and N where T is the thickness of the paper in mm and N is the number of times a paper has to be folded into half.
+
+Constraints
+
+1<=T,N<=10^9
+
+Output Format
+
+Print the answer in modulus of 10^9
+
+Sample Input 0
+
+1 1
+
+Sample Output 0
+
+2
+
