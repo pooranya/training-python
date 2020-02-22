5834f777c56214aa4803b45b8dd6ce86250927c1
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-22 09:02:45 +0530

    Create Rotate Equality.py

---
+++ Rotate Equality.py
@@ -0,0 +1,26 @@
+#question
+Given two strings s1 and s2. The task is to find a string 's2' can be obtained by rotating another string 's1'.
+
+Input Format
+
+The first line of input contains the string s1. The second line of the input contains the string s2
+
+Constraints
+
+Na enna vena kuduppen
+
+image
+
+Output Format
+
+Print 1 if the estring s2 can be fobtained by rotating string s2 by two places else print 0
+
+Sample Input 0
+
+relearnpro
+prorelearn
+
+Sample Output 0
+
+1
+
