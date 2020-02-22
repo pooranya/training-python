c2112bfffcd97d3a615cad06da8026b2293bfe90
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-22 09:01:44 +0530

    Create  FORMING A FRACTION.py

---
+++ FORMING A FRACTION.py
@@ -0,0 +1,41 @@
+#question
+For given two integer 'M' and 'N' find a suitable perfect fraction x/y so that M*(X/Y) will give you the second highest factor of 'N'.
+
+Example: M = 5,N = 64
+
+Second highest factor of N is 32.
+
+so, 5*(32/5) will give you 32.
+
+Ans: 32/5
+
+Input Format
+
+Two space integers 'M' and 'N'
+
+Constraints
+
+0
+
+Output Format
+
+Two numbers 'X' and 'Y' seperated by '/'
+
+Sample Input 0
+
+5 64
+
+Sample Output 0
+
+32/5
+
+Explanation 0
+
+M = 5,N = 64
+
+Second highest factor of N is 32.
+
+so, 5*(32/5) will give you 32.
+
+Ans: 32/5
+
