397e3f544a3cbfed0f4561e76db77288e34a5a64
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-22 17:32:17 +0530

    Create  Flappy Bird 1.py

---
+++ Flappy Bird 1.py
@@ -0,0 +1,23 @@
+A young bird wants to test how much height it can reach at maximum, and the bird rises in height corresponding to the number of times the bird flaps its wings. For the first flap the bird can reach a height of 'X' meters, and for every consecutive flap it climbs by twice the height climbed by the previous flap. And since the bird is a noob in flying, it has a stamina, if it loses it stamina and it stops the flap for a second to take a breath, and in that second it drops by a height in meters equal to the number of consecutive flaps it did just now before taking the breath. The bird young and it can take it can repeat the process only for 'N' times, each time it flaps the wings consecutively and takes a breath. So, find the maximum height to which the bird will reach.
+
+Input Format
+
+First line of input contains an integer N and X with a space between them, where N is the number of times a bird can take a breath and nth time the bird takes a breath, it won’t be able to fly further and X is the distance in meters by which the bird climbs for every first flap after taking the breath. The second line of input will have n spaced integers, where each number corresponds to the number of consecutive flaps did by the bird before it took a breath.
+
+Constraints
+
+1<=N<=10^9 1<=a[i]<=10^9
+
+Output Format
+
+Print the output in modulus of 10^9
+
+Sample Input 0
+
+3 5
+1 2 3
+
+Sample Output 0
+
+29
+
