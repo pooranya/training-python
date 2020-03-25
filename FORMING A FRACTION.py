0d6786f0cd7af49d7ecc595e4b4a65447790a40b
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-25 19:57:58 +0530

    Update FORMING A FRACTION.py

--- FORMING A FRACTION.py
+++ FORMING A FRACTION.py
@@ -38,4 +38,18 @@ Second highest factor of N is 32.
 so, 5*(32/5) will give you 32.
 
 Ans: 32/5
+  ***************************************************************************
+ #answer
+m=10**9
+def pow(n,a):
+    res=1
+    while(a!=0):
+        if(a&1):
+            res=(res*n)%m
+        a=(a>>1)
+        n=(n*n)%m
+    return res
+t,n=map(int,input().split())
+s=t*pow(2,n)
+print(s)
 
